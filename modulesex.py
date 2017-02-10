ffb38f8c311a0436e98cf46b78a3223752293c97
leeon <llhforwork@163.com>
2017-02-10 09:29:43 +0800

    #add module test(fix)

--- modulesex.py
+++ modulesex.py
@@ -6,7 +6,7 @@ Money = 2000
 
 def AddMoney():
     # 想改正代码就取消以下注释:
-    # global Money --sys auto take this var as a local var or a global var ...if you parently write down global(key word),it will be correct.
+    # global Money --sys auto take this var as a local var or a global var ...if you apparently write down global(key word),it will be correct.
     global Money
     Money = Money + 1
 
