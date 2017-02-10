ef52d57ae50579ac786c020d8d6eef8946e9165a
leeon <llhforwork@163.com>
2017-02-10 14:46:33 +0800

    #add file test

---
+++ fileex.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# import related module
+import os
+
+#os.rename("fool.txt","foo.txt")
+
+# open a file
+fo = open("foo.txt", "r+")
+print "file name : ", fo.name
+
+# print fo.write("www.wens.com.cn!")
+str = fo.read();
+print "what is read : ", str
+
+# seek the position
+position = fo.tell();
+print "the position is : ", position
+
+# take the point into the beginning
+position = fo.seek(0, 0)  # the 2nd arg default value is 0
+print "re-get file content : ", fo.read()
+
+#os.rename("foo.txt","fool.txt")
+
+print os.getcwd()
+# close opened file
+fo.close();
